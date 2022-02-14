43c65650b4b17412ea7e3cc6413234b7956ceb74
Markus Bauer <66630178+markus1bauer@users.noreply.github.com>
2022-02-14 15:42:58 +0100

    Update

--- R/show_figure_3_vegetation_height.R
+++ R/show_figure_3_vegetation_height.R
@@ -100,5 +100,5 @@ ggplot(pdata, aes(year, herbHeight, shape = year, ymin = conf.low, ymax = conf.h
 ### Save ###
 ggsave("figure_vegHeight_800dpi_12x8.5cm.tiff",
   dpi = 800, width = 12, height = 8.5, units = "cm",
-  path = here("ouputs", "figures")
+  path = here("outputs", "figures")
 )
